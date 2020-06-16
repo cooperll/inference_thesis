9ae97f73ceea2c176cd6696ce8f1b751c93c6fa7
Cooper Lindsey <cooperlindsey3927@gmail.com>
2020-06-15 23:41:48 -0700

    quick cleanup of main.R

--- main.R
+++ main.R
@@ -1,3 +1,7 @@
+source('~/Developer/inference_thesis/poisson_hep_model.R')
+source('~/Developer/inference_thesis/test_statistics.R')
+source('~/Developer/inference_thesis/power_fns.R')
+
 # initial values as seen in Davison & Sartori's paper
 y = matrix(c(1, 8, 14), nrow=3, ncol=1, byrow=FALSE)
 t = c(27)
@@ -25,6 +29,13 @@ plot(psis, power(psis, psi_0, beta, gamma, lrt, num_reps, alpha),
      type="l", col="blue", xlab=psi_string, ylab="Power", 
      main=title, ylim=ylim)
 
+legend(x=0, y=1.2,
+       legend=c("LRT", "r", "r.boot", "r*_v1", 
+                "r*_v2", "r*_v1 boot", "r*_v2 boot"),
+       col=c("blue", "red", "purple", "dark green", 
+             "dark red", "dark orange", "turquoise"),
+       lty=1:1, cex=0.8)
+
 lines(psis, power(psis, psi_0, beta, gamma, r_psi, num_reps=num_reps, alpha), 
       col="red")
 
@@ -48,10 +59,3 @@ lines(psis, power(psis, psi_0, beta, gamma, r_star_boot_2,
                   num_reps_smaller, alpha, 
                   isBoot=TRUE, num_boot_samples, origData=y), 
       col="turquoise")
-
-legend(x=0, y=1.2,
-       legend=c("LRT", "r", "r.boot", "r*_v1", 
-                "r*_v2", "r*_v1 boot", "r*_v2 boot"),
-       col=c("blue", "red", "purple", "dark green", 
-             "dark red", "dark orange", "turquoise"),
-       lty=1:1, cex=0.8)
